cbab2608d3e16946c002b6669d6743ea936fd72c
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-23 09:03:43 +0900

    complete exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => setShowDetails(!showDetails);
+  const handleLike = (e) =>{
+    console.log('like button pressed for blog', blog);
+  }
+
+  if(showDetails){
+    return (<div className='blog'>
+      {blog.title}<button onClick={toggleDetails}>hide</button> <br/>
+      {blog.url} <br/>
+      likes {blog.likes} <button onClick={handleLike}>like</button> <br/>
+      {blog.author}
+    </div>
+    )
+  }
+  else{
+    return (
+      <div className='blog'>
+        {blog.title} {blog.author} <button onClick={toggleDetails}>view</button>
+      </div>
+    );
+  }
+}
 
 export default Blog
