bb7db33eebfd13c3db003239f0ba9c279fadaf61
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-23 09:07:26 +0900

    complete exercise 5.8

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,16 +4,16 @@ const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false);
 
   const toggleDetails = () => setShowDetails(!showDetails);
-  const handleLike = (e) =>{
+  const handleLike = (e) => {
     console.log('like button pressed for blog', blog);
   }
 
   if(showDetails){
     return (<div className='blog'>
-      {blog.title}<button onClick={toggleDetails}>hide</button> <br/>
+      {blog.title} {blog.author}<button onClick={toggleDetails}>hide</button> <br/>
       {blog.url} <br/>
       likes {blog.likes} <button onClick={handleLike}>like</button> <br/>
-      {blog.author}
+      {blog.user.name}
     </div>
     )
   }
